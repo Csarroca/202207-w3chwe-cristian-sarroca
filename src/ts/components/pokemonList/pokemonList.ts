322d02a97152e411f7a3b9a0e07e571f9c1fb376
CSarroca <sarroca93@gmail.com>
2022-07-22 22:36:22 +0200

    firts pokemon get from apis

--- src/ts/components/pokemonList/pokemonList.ts
+++ src/ts/components/pokemonList/pokemonList.ts
@@ -4,7 +4,6 @@ const getPokemonsFromApi = async () => {
   const pokemons = await fetch(url);
   const data = await pokemons.json();
   return data;
-  console.log(data);
 };
 
 export default getPokemonsFromApi;
