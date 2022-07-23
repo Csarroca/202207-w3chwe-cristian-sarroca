dae26983e4e7087c680db13a3dd1217f201fa8b8
CSarroca <sarroca93@gmail.com>
2022-07-23 12:41:01 +0200

    added interface pokemon por type pokemons list

--- src/ts/components/types/interfaces.ts
+++ src/ts/components/types/interfaces.ts
@@ -3,3 +3,10 @@ export interface Component {
   render?: () => void;
   addListeners?: () => void;
 }
+
+export interface Pokemons {
+  name: string;
+  number: number;
+  imageUrl: string;
+  types: string[];
+}
