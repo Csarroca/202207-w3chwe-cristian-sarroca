80a799c17747ed2a8b7b74fef3115511a04eaec0
CSarroca <sarroca93@gmail.com>
2022-07-23 14:11:34 +0200

    added pokemon interface por typing pokelist for api fetch

--- src/utilities/pokemonList/pokemonList.ts
+++ src/utilities/pokemonList/pokemonList.ts
@@ -1,19 +1,19 @@
+/*
 import { IPokemonData } from "../../ts/components/types/interfaces.js";
 
 const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10";
-
 const getPokemonsFromApi = async (): Promise<void> => {
   const pokemons = await fetch(url);
   const data: IPokemonData = await pokemons.json();
-  /*
+
   const pokemonsList: object[] = [];
   Object.values(data.results).forEach(async (pokemon: object) => {
     const resolve = await fetch(pokemon.url);
     const pokemonData = await resolve.json();
     pokemonsList.push(pokemonData);
   });
-  return pokemonsList; */
-  console.log(data);
 };
 getPokemonsFromApi();
 export default getPokemonsFromApi;
+
+return pokemonsList; */
