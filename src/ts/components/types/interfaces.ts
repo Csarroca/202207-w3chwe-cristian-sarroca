8fa1cc73445a2d2c0440d5fe820f3b222f27e8ba
CSarroca <sarroca93@gmail.com>
2022-07-23 14:10:28 +0200

    added pokemon interface por typing pokelist for api fetch

--- src/ts/components/types/interfaces.ts
+++ src/ts/components/types/interfaces.ts
@@ -10,3 +10,13 @@ export interface IPokemon {
   imageUrl: string;
   types: string[];
 }
+
+export interface IPokemonData {
+  count: number;
+  next: string;
+  previous: null | string;
+  results: {
+    name: string;
+    url: string;
+  }[];
+}
